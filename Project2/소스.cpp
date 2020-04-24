a6c014b4a7085a08e6771a7d15207cc17c3e2b3a
JUNGSUKWON <jsw782@naver.com>
2020-04-24 17:19:38 +0900

    백준문제

---
+++ Project2/소스.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+
+struct INFO {
+	int ry, rx, by, bx, count;
+
+};
+INFO start;
+char map[10][11];
+
+int main_13460()
+{
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; ++i); {
+
+		scanf("%s", map[i]);
+
+	}
+	for (int y = 0; y < n; ++y) {
+
+
+
+
+	}
+
+
+	return 0;
+
+
+
+
+
+
+}
+
+
+
